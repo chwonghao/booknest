39207b6865d908bea2b5f8a621243a7cf8a91967
Nguyễn Chương Hào <102464075+chwonghao@users.noreply.github.com>
2025-11-20 14:06:04 +0700

    Thêm WebConfig cho product-service, order-service, payment-service và product-service

---
+++ payment-service/src/main/java/com/booknest/paymentservice/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.booknest.paymentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig {
+
+  @Bean
+  public CorsFilter corsFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080"));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setExposedHeaders(List.of("Location"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+}
