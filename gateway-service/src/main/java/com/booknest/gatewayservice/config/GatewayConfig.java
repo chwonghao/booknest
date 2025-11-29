746bc6e175977ef299a14f6a7fec2e109c54de1c
Nguyễn Chương Hào <102464075+chwonghao@users.noreply.github.com>
2025-11-29 08:21:36 +0700

    chore(gateway-service): cập nhật GatewayConfig và application.yml

--- gateway-service/src/main/java/com/booknest/gatewayservice/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/booknest/gatewayservice/config/GatewayConfig.java
@@ -12,7 +12,9 @@ public class GatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
             .route("user-service", r -> r.path("/api/users/**").uri("lb://user-service"))
+            .route("auth-service", r -> r.path("/api/auth/**").uri("lb://user-service"))
             .route("product-service", r -> r.path("/api/products/**").uri("lb://product-service"))
+            .route("product-service-categories", r -> r.path("/api/categories/**").uri("lb://product-service"))
             .route("order-service", r -> r.path("/api/orders/**").uri("lb://order-service"))
             .route("payment-service", r -> r.path("/api/payments/**").uri("lb://payment-service"))
             .route("notification-service", r -> r.path("/api/notifications/**").uri("lb://notification-service"))
