de259f1cbd032834068dd31269fbee07627871b1
Nguyễn Chương Hào <102464075+chwonghao@users.noreply.github.com>
2025-12-02 19:05:04 +0700

    refactor(user-service): remove legacy controllers, configs, models, security and repository

--- user-service/src/main/java/com/booknest/userservice/model/Role.java
+++
@@ -1,5 +0,0 @@
-package com.booknest.userservice.model;
-
-public enum Role {
-    USER, ADMIN, SELLER
-}
--- user-service/src/main/java/com/booknest/userservice/model/Status.java
+++
@@ -1,5 +0,0 @@
-package com.booknest.userservice.model;
-
-public enum Status {
-    ACTIVE, INACTIVE, BANNED
-}
